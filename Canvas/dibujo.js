e52fdaa2319aa66a74b2919af3abfb974849be18
Gonzalo Verdia <gonzaloverdiaromero@gmail.com>
2020-02-21 13:29:34 -0300

    Canvas 1.1 (agregando funcion para optimizar el codigo)

--- Canvas/dibujo.js
+++ Canvas/dibujo.js
@@ -1,10 +1,18 @@
 var d = document.getElementById("dibujito")
 var lienzo = d.getContext("2d");
 
+dibujarLinea("pink", 10, 300, 220, 10);
+dibujarLinea("yellow", 300, 10, 10, 220);
+
+function dibujarLinea(color, xini, yini, xfin, yfin)
+{
+
 lienzo.beginPath(); // Comenzamos a dibujar
-lienzo.strokeStyle = "red"; // color del trazo
-lienzo.moveTo(100, 100); // posicion inicial
-lienzo.lineTo(200, 200); // mover a... creando linea
+lienzo.strokeStyle = color; // color del trazo
+lienzo.moveTo(xini, yini); // posicion inicial
+lienzo.lineTo(xfin, yfin); // mover a... creando linea
 lienzo.stroke(); // cerramos linea
 lienzo.closePath(); // Terminamos de dibujar 
+	
+}
 
