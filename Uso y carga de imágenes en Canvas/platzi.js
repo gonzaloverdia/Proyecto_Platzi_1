ddd556a95e56e19d2d0574a2d757996ad99ac6b6
Gonzalo Verdia <gonzaloverdiaromero@gmail.com>
2020-02-25 16:01:50 -0300

    Uso y carga de imágenes en Canvas 1.1 (Agregados elemenos a cargar)

--- Uso y carga de imágenes en Canvas/platzi.js
+++ Uso y carga de imágenes en Canvas/platzi.js
@@ -9,6 +9,14 @@ var vaca = {
 	url: "src/vaca.png",
 	cargaOk: false
 }
+var cerdo = {
+	url: "src/cerdo.png",
+	cargaOk: false
+}
+var pollo = {
+	url: "src/pollo.png",
+	cargaOk: false
+}
 
 // Cargar al canvas la ruta.
 fondo.imagen = new Image(); 
@@ -18,6 +26,14 @@ fondo.imagen.addEventListener("load", cargarFondo);
 vaca.imagen = new Image();
 vaca.imagen.src = vaca.url;
 vaca.imagen.addEventListener("load", cargarVaca);
+
+cerdo.imagen = new Image();
+cerdo.imagen.src = cerdo.url;
+cerdo.imagen.addEventListener("load", cargarCerdo);
+
+pollo.imagen = new Image();
+pollo.imagen.src = pollo.url;
+pollo.imagen.addEventListener("load", cargarPollo);
 
 function cargarFondo()
 {
@@ -29,6 +45,19 @@ function cargarVaca()
 	vaca.cargaOk = true;
 	dibujar();
 }
+
+function cargarCerdo()
+{
+	cerdo.cargaOk = true;
+	dibujar();
+}
+
+function cargarPollo()
+{
+	pollo.cargaOk = true;
+	dibujar();
+}
+
 function dibujar()
 {
 	if(fondo.cargaOk == true)
@@ -36,7 +65,7 @@ function dibujar()
 		papel.drawImage(fondo.imagen, 0, 0);
 	}
 	if(vaca.cargaOk == true)
-	{	var cantidad = aleatorio(5, 25);
+	{	var cantidad = aleatorio(2, 7);
 	 	
 		for (i = 0; i<cantidad; i++)
 		{
@@ -45,6 +74,27 @@ function dibujar()
 		papel.drawImage(vaca.imagen, x, y);		
 		}
 	console.log(cantidad);
+	}
+	if(cerdo.cargaOk == true)
+	{	var cantidad = aleatorio(2, 7);
+	 	
+		for (i = 0; i<cantidad; i++)
+		{
+		var x = aleatorio(0, 420);
+		var y = aleatorio(0, 420);
+		papel.drawImage(cerdo.imagen, x, y);		
+		}
+
+	}
+	if(pollo.cargaOk == true)
+	{	var cantidad = aleatorio(2, 7);
+	 	
+		for (i = 0; i<cantidad; i++)
+		{
+		var x = aleatorio(0, 420);
+		var y = aleatorio(0, 420);
+		papel.drawImage(pollo.imagen, x, y);		
+		}
 	}
 }
 
